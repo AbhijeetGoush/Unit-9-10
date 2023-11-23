1bc0e479cf6b44b7e85d341650456f618ef7b9e5
AbhijeetGoush <530082@chrome.escg.ac.uk>
2023-11-23 14:59:09 +0000

    Boundaries

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    Transform playerTrans;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerTrans = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(playerTrans.position.x >= 10.788f)
+        {
+            playerTrans.transform.position = new Vector2(-10.683f, playerTrans.position.y);
+        }
+        if(playerTrans.position.x <= -10.788)
+        {
+            playerTrans.transform.position = new Vector2(10.683f, playerTrans.position.y);
+        }
+        if(playerTrans.position.y >= 6.4f)
+        {
+            playerTrans.transform.position = new Vector2(playerTrans.position.x, -6);
+        }
+        if (playerTrans.position.y <= -6.4f)
+        {
+            playerTrans.transform.position = new Vector2(playerTrans.position.x, 6);
+        }
     }
 }
